0de48c90d54b19b475c2f8bc52663f64c4ee7acc
jroca72 <jroca72@gmail.com>
2016-04-05 13:45:15 +0200

    primer template prueba

--- lar/lar/settings.py
+++ lar/lar/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'lar.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [(os.path.join(BASE_DIR, './templates').replace('\\', '/'),)],
+        'DIRS': [(os.path.join(BASE_DIR, './templates'))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
