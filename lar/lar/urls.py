221df3f46075fb91c72418afbe4cbe7ed249bc19
jroca72 <jroca72@gmail.com>
2016-04-06 16:20:13 +0200

    portada libros

--- lar/lar/urls.py
+++ lar/lar/urls.py
@@ -13,11 +13,23 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
+from django.views.static import serve
 from django.contrib import admin
+from django.conf import settings
 from libros import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),
+    url(r'^libros_index/', views.libros_index, name = 'libros_index'),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += [
+        url(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    ]
--- lar/libros/views.py
+++ lar/libros/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
+
 from libros.models import autores, titulos, lectores
 
 # Create your views here.
 def home(request):
 	return render(request, 'libros/home.html')
+	
+def libros_index(request):
+ 	los_libros = titulos.objects.all()
+	context = {'los_libros': los_libros }
+	return render(request, 'libros/index.html', context)
