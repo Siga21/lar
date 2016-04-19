9b75e838bfc173f220a80c2c77ae41898c02d808
jroca72 <jroca72@gmail.com>
2016-04-19 12:08:53 +0200

    XXX

--- lar/lar/wsgi.py
+++ lar/lar/wsgi.py
@@ -7,10 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+
+
+sys.path.append("/home/roca67/lar/lar")
+os.environ["DJANGO_SETTINGS_MODULE"] = "lar.settings"
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lar.settings")
-
 application = get_wsgi_application()
